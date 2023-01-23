6aebbac4926e65f3e505194ff7f370f5fa758ae8
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2023-01-23 04:05:44 +0100

    little test

--- Circle.java
+++ Circle.java
@@ -1,31 +1,64 @@
 /**
  * The Circle class models a circle with a radius and color.
  */
-public class Circle {  // Save as "Circle.java"
+public class Circle { // Save as "Circle.java"
    // private instance variable, not accessible from outside this class
    private double radius;
    private String color;
-   
+
    // Constructors (overloaded)
    /** Constructs a Circle instance with default value for radius and color */
-   public Circle() {  // 1st (default) constructor
+   public Circle() { // 1st (default) constructor
       radius = 1.0;
       color = "red";
    }
-   
+
    /** Constructs a Circle instance with the given radius and default color */
-   public Circle(double r) {  // 2nd constructor
-      radius = r;
+   public Circle(double radius) { // 2nd constructor
+      this.radius = radius;
       color = "red";
    }
-   
+
+   // 3rd constructor to construct a new instance of Circle with the given radius
+   // and color
+   public Circle(double radius, String color) {
+      this.radius = radius;
+      this.color = "red";
+   }
+
    /** Returns the radius */
    public double getRadius() {
-     return radius; 
+      return this.radius;
    }
-   
+
    /** Returns the area of this Circle instance */
    public double getArea() {
-      return radius*radius*Math.PI;
+      return this.radius * this.radius * Math.PI;
    }
+
+   // Getter for instance variable color
+   public String getColor() {
+      return this.color;
+   }
+
+   
+   // calculate circumference 
+   public String getCircumference() {
+      return "circumference";
+   }
+
+   // Setter for instance variable radius
+public void setRadius(double newRadius) {
+   this.radius = newRadius;
+}
+ 
+// Setter for instance variable color
+public void setColor(String newColor) { 
+   this.color = newColor;
+ }
+
+ /** Return a self-descriptive string of this instance in the form of Circle[radius=?,color=?] */
+public String toString() {
+   return "Circle[radius=" + radius + " color=" + color + "]";
+}
 }
